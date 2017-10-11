34755bb9b5c23039255c5260bc00d21f17f76508
dfsims <darren.sims929@gmail.com>
2017-10-11 15:45:56 -0700

    Changed some stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,20 +2,26 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXMLLoader;
 
 import javax.swing.*;
+//import java.awt.;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.LinkedList;
+
 
 public class Controller extends Main implements Initializable {
 
+
+    //private javafx.scene.control.TextField UsernameField;
     @FXML
     private RadioButton  Q1A;
     @FXML
@@ -38,201 +44,337 @@ public class Controller extends Main implements Initializable {
     private int answered = 0;
     @FXML
     private Button SubmitAll;
+    @FXML
+    private TextField UsernameField;
+    @FXML
+    private PasswordField passfield;
 
+
+
+
+
+
+    Image hamburger = new Image("/resources/hamburger.png");
+    Image userIcon = new Image("/resources/usericon.png");
+    Image logo = new Image("/resources/dsdt.png");
+    @FXML
+    ImageView hamburgerView = new ImageView();
+    @FXML
+    ImageView userView = new ImageView();
+    @FXML
+    ImageView logoView = new ImageView();
+    @FXML
+    ImageView logoView1 = new ImageView();
+
+
+
+    Lesson lesson = new Lesson(0);
+
+    private  String userInput = null;
+    private String passInput = null;
+    private Boolean truelogin = false;
+    UserList linkedlist = new sample.UserList();
+    JFrame frame = new JFrame();
+
+
+
+    @FXML
+    Pane sideMenuPane, lessonMenuPane, loginPane, gradePane, quizPane, lessonPane, mainMenuPane ,lessonPane2,lessonPane3, lessonPane4= new Pane();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        hamburgerView.setImage(hamburger);
+        userView.setImage(userIcon);
+        logoView.setImage(logo);
+        logoView1.setImage(logo);
+        switchPane("logIn");
     }
+
     @FXML
 
-    public void loginButtonHandler(ActionEvent event) throws Exception{
-        Parent newSceneRoot = FXMLLoader.load(getClass().getResource("lesson1.fxml"));
-        Scene newScene = new Scene(newSceneRoot);
-
-        Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
-        window.setScene(newScene);
-        window.show();
-    }
-
-    public void nextLesson1Handler(ActionEvent event) throws Exception{
-        Parent newSceneRoot = FXMLLoader.load(getClass().getResource("lesson2.fxml"));
-        Scene newScene = new Scene(newSceneRoot,640,400);
-
-        Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
-        window.setScene(newScene);
-        window.show();
-    }
-
-    public void nextLesson2Handler(ActionEvent event) throws Exception{
-        Parent newSceneRoot = FXMLLoader.load(getClass().getResource("lesson3.fxml"));
-        Scene newScene = new Scene(newSceneRoot,640,400);
-
-        Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
-        window.setScene(newScene);
-        window.show();
-    }
-
-    public void nextLesson3Handler(ActionEvent event) throws Exception{
-        Parent newSceneRoot = FXMLLoader.load(getClass().getResource("lesson4.fxml"));
-        Scene newScene = new Scene(newSceneRoot,640,400);
-
-        Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
-        window.setScene(newScene);
-        window.show();
-    }
-
-    public void nextLesson4Handler(ActionEvent event) throws Exception{
-        Parent newSceneRoot = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Scene newScene = new Scene(newSceneRoot,640,400);
-
-        Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
-        window.setScene(newScene);
-        window.show();
-    }
-    @FXML
-    public void questionAnswer(ActionEvent e)
+    public void loginButtonHandler(ActionEvent event) throws Exception
     {
-        if(e.getSource()== Q1A)
+        userInput = UsernameField.getText();
+        passInput = passfield.getText();
+        node temp = linkedlist.getNode();
+        node first = temp;
+        if(linkedlist.isEmpty()== true)
         {
-            answered++;
-            Q1B.setDisable(true);
-            Q1C.setDisable(true);
+            Alert alert = new Alert((Alert.AlertType.ERROR));
+            alert.setTitle("Users");
+            alert.setHeaderText("No accounts registered");
+            alert.showAndWait();
         }
-        else if(e.getSource()== Q1B)
+        else
         {
-            answered++;
-            Q1A.setDisable(true);
-            Q1C.setDisable(true);
-        }
-        else if(e.getSource()== Q1C)
-        {
-            answered++;
-            Q1B.setDisable(true);
-            Q1A.setDisable(true);
-        }
-        else if(e.getSource()== Q2A)
-        {
-            answered++;
-            Q2B.setDisable(true);
-            Q2C.setDisable(true);
-        }
-        else if(e.getSource()== Q2B)
-        {
-            answered++;
-            Q2A.setDisable(true);
-            Q2C.setDisable(true);
-        }
-        else if(e.getSource()== Q2C)
-        {
-            answered++;
-            Q2B.setDisable(true);
-            Q2A.setDisable(true);
-        }
-        else if(e.getSource()== Q3A)
-        {
-            answered++;
-            Q3B.setDisable(true);
-            Q3C.setDisable(true);
-        }
-        else if(e.getSource()== Q3B)
-        {
-            answered++;
-            Q3A.setDisable(true);
-            Q3C.setDisable(true);
-        }
-        else if(e.getSource()== Q3C)
-        {
-            answered++;
-            Q3B.setDisable(true);
-            Q3A.setDisable(true);
-        }
 
-    }
-    @FXML
-    public void submitAllHandler(ActionEvent event) throws Exception
-    {
-        if(answered >= 3)
-        {
-            if(!Q1B.isDisabled() && !Q2A.isDisabled() && !Q3B.isDisabled())
+            while (temp != null)
             {
+                if (temp.userName.equals(userInput))
+                {
+                    if (temp.password.equals(passInput)) {
+                        truelogin = true;
+                        switchPane("mainMenu");
+                    }
+                    else
+                    {
+                        Alert alert = new Alert((Alert.AlertType.ERROR));
+                        alert.setTitle("Acount Not Found");
+                        alert.setHeaderText("Incorrect Password");
+                        alert.showAndWait();
+
+                    }
+                }
+                else if(truelogin == false)
+                {
+                    Alert alert = new Alert((Alert.AlertType.ERROR));
+                    alert.setTitle("Found?");
+                    alert.setHeaderText("Account not found");
+                    alert.showAndWait();
+                }
+                temp = temp.next;
+            }
+            truelogin = false;
+
+
+        }
+
+
+    }
+
+
+    public void createAccountHandler() throws Exception
+    {
+        userInput = UsernameField.getText();
+        passInput = passfield.getText();
+        linkedlist.add(userInput, passInput);
+        Alert alert = new Alert((Alert.AlertType.CONFIRMATION));
+        alert.setTitle("Confirmed");
+        alert.setHeaderText("Account Created");
+        alert.showAndWait();
+        linkedlist.printUsers();
+    }
+
+    @FXML
+    public void nextLesson1Handler(ActionEvent event)
+    {
+        switchPane("lessonPane2");
+        lessonPane2.setVisible(true);
+        lessonPane.setVisible(false);
+    }
+
+    public void nextLesson2Handler(ActionEvent event)
+    {
+        switchPane("lessonPane3");
+        lessonPane3.setVisible(true);
+        lessonPane2.setVisible(false);
+    }
+
+    public void nextLesson3Handler(ActionEvent event) {
+        switchPane("lessonPane4");
+        lessonPane4.setVisible(true);
+        lessonPane3.setVisible(false);
+    }
+
+    public void nextLesson4Handler(ActionEvent event)  {
+        switchPane("mainMenu");
+        mainMenuPane.setVisible(true);
+        lessonPane4.setVisible(false);
+    }
+
+    //Side Menu
+    @FXML
+    public void sideMenuButtonHandler() throws Exception {
+        sideMenuPane.setVisible(true);
+    }
+
+    @FXML
+    public void closeButtonHandler() throws Exception {
+        sideMenuPane.setVisible(false);
+    }
+
+    @FXML
+    public void quizHandler() {
+        switchPane("quiz");
+    }
+
+    @FXML
+    public void gradeHandler() {
+        switchPane("grades");
+
+    }
+
+    @FXML
+    public void logoutHandler() {
+        switchPane("logIn");
+    }
+
+    @FXML
+    public void lessonMenuHandler() {
+        switchPane("lessonMenu");
+    }
+
+    //Lessons
+    @FXML
+    public void productRuleHandler() {
+        switchPane("lessons");
+        lesson.setLesson(0);
+    }
+
+    @FXML
+    public void powerRuleHandler() {
+        switchPane("lessons");
+
+    }
+
+    @FXML
+    public void chainRuleHandler() {
+        switchPane("lessons");
+
+    }
+
+    @FXML
+    public void exponentRuleHandler() {
+        switchPane("lessons");
+
+    }
+
+    @FXML
+    public void questionAnswer(ActionEvent e) {
+        if (e.getSource() == Q1A) {
+            answered++;
+            Q1B.setDisable(true);
+            Q1C.setDisable(true);
+        } else if (e.getSource() == Q1B) {
+            answered++;
+            Q1A.setDisable(true);
+            Q1C.setDisable(true);
+        } else if (e.getSource() == Q1C) {
+            answered++;
+            Q1B.setDisable(true);
+            Q1A.setDisable(true);
+        } else if (e.getSource() == Q2A) {
+            answered++;
+            Q2B.setDisable(true);
+            Q2C.setDisable(true);
+        } else if (e.getSource() == Q2B) {
+            answered++;
+            Q2A.setDisable(true);
+            Q2C.setDisable(true);
+        } else if (e.getSource() == Q2C) {
+            answered++;
+            Q2B.setDisable(true);
+            Q2A.setDisable(true);
+        } else if (e.getSource() == Q3A) {
+            answered++;
+            Q3B.setDisable(true);
+            Q3C.setDisable(true);
+        } else if (e.getSource() == Q3B) {
+            answered++;
+            Q3A.setDisable(true);
+            Q3C.setDisable(true);
+        } else if (e.getSource() == Q3C) {
+            answered++;
+            Q3B.setDisable(true);
+            Q3A.setDisable(true);
+        }
+
+    }
+
+    @FXML
+    public void submitAllHandler(ActionEvent event) throws Exception {
+        if (answered >= 3) {
+            if (!Q1B.isDisabled() && !Q2A.isDisabled() && !Q3B.isDisabled()) {
                 displayReaction(3);
-            }
-            else if((!Q1B.isDisabled() && !Q2A.isDisabled())||(!Q2A.isDisabled() && !Q3B.isDisabled())|| (!Q1A.isDisabled() && !Q3B.isDisabled()) )
-            {
+            } else if ((!Q1B.isDisabled() && !Q2A.isDisabled()) || (!Q2A.isDisabled() && !Q3B.isDisabled()) || (!Q1A.isDisabled() && !Q3B.isDisabled())) {
                 displayReaction(2);
-            }
-            else if(!Q1B.isDisabled() || !Q2A.isDisabled() || !Q3B.isDisabled())
-            {
+            } else if (!Q1B.isDisabled() || !Q2A.isDisabled() || !Q3B.isDisabled()) {
                 displayReaction(1);
-            }
-            else
+            } else
                 displayReaction(0);
         }
     }
+
     @FXML
-    public void logoutHandler(ActionEvent actionEvent)
-    {
+    public void changLessonHandler(ActionEvent actionEvent) {
     }
+
     @FXML
-    public void changLessonHandler(ActionEvent actionEvent)
-    {
+    public void changeAssignmentHandler(ActionEvent actionEvent) {
     }
+
     @FXML
-    public void changeAssignmentHandler(ActionEvent actionEvent)
-    {
+    public void squizzesHandler(ActionEvent actionEvent) {
     }
+
     @FXML
-    public void squizzesHandler(ActionEvent actionEvent)
-    {
-    }
-    @FXML
-    public void newLesson1Handler(ActionEvent event) throws Exception
-    {
+    public void newLesson1Handler(ActionEvent event) throws Exception {
         Parent newSceneRoot = FXMLLoader.load(getClass().getResource("lesson1.fxml"));
         Scene newScene = new Scene(newSceneRoot);
 
-        Stage window = (Stage) (((Node)event.getSource()).getScene().getWindow());
+        Stage window = (Stage) (((Node) event.getSource()).getScene().getWindow());
         window.setScene(newScene);
         window.show();
     }
-    public void displayReaction(int correct) throws Exception
-    {
-        if(correct == 3)
-        {
+
+    public void displayReaction(int correct) throws Exception {
+        if (correct == 3) {
             Parent newSceneRoot = FXMLLoader.load(getClass().getResource("GreatJob.fxml"));
-            Scene newScene = new Scene(newSceneRoot,640,400);
+            Scene newScene = new Scene(newSceneRoot, 640, 400);
 
             Stage window = (Stage) (SubmitAll.getScene().getWindow());
             window.setScene(newScene);
             window.show();
-        }
-        else if(correct == 2)
-        {
+        } else if (correct == 2) {
             Parent newSceneRoot = FXMLLoader.load(getClass().getResource("NotQuite.fxml"));
-            Scene newScene = new Scene(newSceneRoot,640,400);
+            Scene newScene = new Scene(newSceneRoot, 640, 400);
 
             Stage window = (Stage) (SubmitAll.getScene().getWindow());
             window.setScene(newScene);
             window.show();
-        }
-        else if(correct == 1)
-        {
+        } else if (correct == 1) {
             Parent newSceneRoot = FXMLLoader.load(getClass().getResource("NoBueno.fxml"));
-            Scene newScene = new Scene(newSceneRoot,640,400);
+            Scene newScene = new Scene(newSceneRoot, 640, 400);
 
             Stage window = (Stage) (SubmitAll.getScene().getWindow());
             window.setScene(newScene);
             window.show();
-        }
-        else if(correct == 0 )
-        {
+        } else if (correct == 0) {
             Parent newSceneRoot = FXMLLoader.load(getClass().getResource("NoBueno2.fxml"));
-            Scene newScene = new Scene(newSceneRoot,640,400);
+            Scene newScene = new Scene(newSceneRoot, 640, 400);
 
             Stage window = (Stage) (SubmitAll.getScene().getWindow());
             window.setScene(newScene);
             window.show();
         }
+    }
+
+    //switchPanes
+    public void switchPane(String visiblePane) {
+        Boolean logIn = false, lessons = false, quiz = false, grades = false, lessonMenu = false, mainMenu = false;
+        if (visiblePane.equals("logIn")) {
+            logIn = true;
+        }
+        if (visiblePane.equals("lessonMenu")) {
+            lessonMenu = true;
+        }
+        if (visiblePane.equals("quiz")) {
+            quiz = true;
+        }
+        if (visiblePane.equals("grades")) {
+            grades = true;
+        }
+        if (visiblePane.equals("lessons")) {
+            lessons = true;
+        }
+        if (visiblePane.equals("mainMenu")) {
+            mainMenu = true;
+        }
+        lessonPane.setVisible(lessons);
+        lessonMenuPane.setVisible(lessonMenu);
+        loginPane.setVisible(logIn);
+        quizPane.setVisible(quiz);
+        gradePane.setVisible(grades);
+        mainMenuPane.setVisible(mainMenu);
     }
 }
--- src/sample/Lesson.java
+++ src/sample/Lesson.java
@@ -6,16 +6,7 @@ import javafx.scene.image.ImageView;
 import java.awt.*;
 
 public class Lesson {
-<<<<<<< HEAD
-    Image lesson1 = new Image("/resources/Lesson1.png");
-    Image lesson2 = new Image("/resources/lesson2.png");
-    //Image lesson3 = new Image("");
-    //Image lesson4 = new Image("");
-    ImageView lessonImage;
 
-    public void Lesson(int number){
-        ImageView lessonImage = new ImageView("lesson"+number);
-=======
     private Image lesson1 = new Image("/resources/Lesson1.png");
 //    Image lesson2 = new Image("");
   //  Image lesson3 = new Image("");
@@ -39,17 +30,18 @@ public class Lesson {
 //                lessonImage = lesson4;
 //                break;
         }
->>>>>>> origin/master
     }
     public void setLesson(int number)
     {
 
     }
-<<<<<<< HEAD
+
     public ImageView getLessonImage()
-=======
+    {
+
+    }
+
     public Image getLessonImage()
->>>>>>> origin/master
     {
         return lessonImage;
     }
